672b939ddef935f4311173f4b17fe4532d4f2d0f
Mark Brown <broonie@opensource.wolfsonmicro.com>
2012-08-31 10:36:37 -0700

    regulator: core: Support bypass mode
    Many regulators support a bypass mode where they simply switch their
    input supply to the output. This is mainly used in low power retention
    states where power consumption is extremely low so higher voltage or
    less clean supplies can be used.
    
    Support this by providing ops for the drivers and a consumer API which
    allows the device to be put into bypass mode if all consumers enable it
    and the machine enables permission for this.
    
    This is not supported as a mode since the existing modes are rarely used
    due to fuzzy definition and mostly redundant with modern hardware which is
    able to respond promptly to load changes.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>
    Reviewed-by: Graeme Gregory <gg@slimlogic.co.uk>

--- include/linux/regulator/consumer.h
+++ include/linux/regulator/consumer.h
@@ -176,6 +176,8 @@ int regulator_set_mode(struct regulator *regulator, unsigned int mode);
 unsigned int regulator_get_mode(struct regulator *regulator);
 int regulator_set_optimum_mode(struct regulator *regulator, int load_uA);
 
+int regulator_allow_bypass(struct regulator *regulator, bool allow);
+
 /* regulator notifier block */
 int regulator_register_notifier(struct regulator *regulator,
 			      struct notifier_block *nb);
@@ -304,6 +306,12 @@ static inline int regulator_set_optimum_mode(struct regulator *regulator,
 					int load_uA)
 {
 	return REGULATOR_MODE_NORMAL;
+}
+
+static inline int regulator_allow_bypass(struct regulator *regulator,
+					 bool allow)
+{
+	return 0;
 }
 
 static inline int regulator_register_notifier(struct regulator *regulator,
